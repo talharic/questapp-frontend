cb5cc6b829deeeac6ca0dcea7aa9b5b5c883234b
Talha Arıç <talharic@gmail.com>
2022-03-03 00:06:19 +0300

    router and component impl.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Post from '../src/components/Post/Post';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
